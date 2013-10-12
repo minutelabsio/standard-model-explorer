2c450048ea474383745f0200149e8855e8a051fe
Jasper <wellcaffeinated@iCoffee.local>
2013-10-12 00:11:24 -0500

    added fonts. better colors. fixed transition bug

--- app/library/js/modules/visualizations/interactions.js
+++ app/library/js/modules/visualizations/interactions.js
@@ -61,39 +61,53 @@ define(
                 var colors = d3.scale.category10();
                 var regions = self.svg.append('g').attr('class', 'regions');
 
-                regions.selectAll('.region')
+                var regionEls = regions.selectAll('.region')
                     .data( regionData )
                     .enter()
                     .append('circle')
                     .attr('class', 'region')
                     .attr('r', '0%')
                     .attr('fill', function( d, idx ){
-                        return d3.rgb( colors( idx ) ).brighter( 1 );
+                        var c = d3.hsl( colors( idx ) ).brighter( 0.8 );
+                        d.color = c;
+                        return 'hsla('+[c.h, c.s*100+'%', c.l*100 + '%'].join(',')+', 0.25)';
                     })
-                    .style('opacity', 0.25)
+                    // .style('opacity', 0.25)
                     .attr('cx', function( d ){ return d.x; })
                     .attr('cy', function( d ){ return d.y; })
-                    .on('mouseover', function( d, idx ){
-                        var el = this;
-                        regions.append(function(){ return el; });
-                        d3.select(this)
-                            .transition()
-                            .ease('ease-out')
-                            .duration(300)
-                            .style('opacity', 1)
-                            ;
-                    })
-                    .on('mouseout', function( d, idx ){
-                        d3.select(this)
-                            .transition()
-                            .ease('ease-out')
-                            .duration(300)
-                            .style('opacity', 0.25)
-                            ;
-                    })
-                  .transition()
+                    ;
+
+                regionEls.transition()
                     .duration(500)
                     .attr('r', function( d ){ return d.r; })
+                    .each('end', function(){
+                        regionEls.on('mouseover', function( d, idx ){
+                            var el = this;
+                            regions.append(function(){ return el; });
+                            d3.select(this)
+                                .transition()
+                                .ease('ease-out')
+                                .duration(300)
+                                // .style('opacity', 1)
+                                .attr('fill', function( d ){
+                                    var c = d.color.brighter( 0.3 );
+                                    return 'hsla('+[c.h, c.s*100+'%', c.l*100 + '%'].join(',')+', 1)';
+                                })
+                                ;
+                        })
+                        .on('mouseout', function( d, idx ){
+                            d3.select(this)
+                                .transition()
+                                .ease('ease-out')
+                                .duration(300)
+                                // .style('opacity', 0.25)
+                                .attr('fill', function( d ){
+                                    var c = d.color;
+                                    return 'hsla('+[c.h, c.s*100+'%', c.l*100 + '%'].join(',')+', 0.25)';
+                                })
+                                ;
+                        });
+                    })
                     ;
             },
 
@@ -102,13 +116,13 @@ define(
                 var self = this;
 
                 var labelData = [
-                    { name: 'W', x: '50%', y: '50%' },
-                    { name: 'H', x: '50%', y: '68%' },
+                    { name: 'W', x: '50%', y: '50%', self: true },
+                    { name: 'H', x: '50%', y: '68%', self: true },
                     { name: 'Z', x: '50%', y: '78%' },
                     { name: 'q', x: '30%', y: '50%' },
                     { name: 'l', x: '70%', y: '50%' },
                     { name: 'γ', x: '50%', y: '30%' },
-                    { name: 'g', x: '10%', y: '50%' }
+                    { name: 'g', x: '10%', y: '50%', self: true }
                 ];
                 var labels = self.svg.append('g').attr('class', 'labels');
 
@@ -121,7 +135,13 @@ define(
 
                 labelsSel.append('circle')
                     .attr('fill', 'rgba(240, 240, 240, 0.7)')
-                    .attr('stroke', 'rgba(200, 200, 200, 0.7)')
+                    .attr('stroke', function( d ){
+                        return d.self ? 'rgba(70, 70, 70, 0.7)' : 'rgba(200, 200, 200, 0.7)';
+                    })
+                    .attr('stroke-dasharray', '8,8')
+                    .attr('stroke-width', function( d ){
+                        return d.self ? 3 : 1;
+                    })
                     .attr('r', '0.8em')
                     .attr('cx', function( d ){ return d.x; })
                     .attr('cy', function( d ){ return d.y; })
